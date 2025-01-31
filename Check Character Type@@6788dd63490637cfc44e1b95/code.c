4608fade7074546ca0b013782815ef618fe2fcc6
ManthanGarg-1408 <manthan1408.becse24@chitkara.edu.in>
2025-01-31 22:38:25 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main() {
     if (isdigit(ch)){printf("Digit");}
     else if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
     ch=='A' || ch=='E' || ch=='I' ||ch=='O' || ch=='U'){printf("Vowel");}
-    else if (ch>='a' && ch<='z') || (ch>='A' && ch<='Z'){
+    else if ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
         printf("Consonant");
     }
     else{
