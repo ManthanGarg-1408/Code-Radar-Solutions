7d4986bd39c74fb4f6392d8503cef98a00fccfd9
Manthan Garg <manthangarg2024cse@gmail.com>
2025-03-30 15:41:47 +0530

    Solution to Smallest Window Sort

---
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -0,0 +1,23 @@
+void findUnsortedsubArray(int arr[],int n){
+    int swapped;
+    for(int i=0;i<n-1;i++){
+        swapped = 0;
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = 1;
+            }
+        }
+        if(swapped == 0){
+            return;
+        }
+    }
+}
+
+void  arrayprint(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
